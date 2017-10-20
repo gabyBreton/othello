137c23268990b17b08f48c74eb1870e6deed26ad
Gabriel Breton <Breton@43397>
2017-10-20 23:16:07 +0200

    Add enum Color.

---
+++ Projet-Othello-Breton/src/projet/othello/breton/model/Color.java
@@ -0,0 +1,12 @@
+package projet.othello.breton.model;
+
+/**
+ * Enumeration of colors that are needed to identify each of the two players.
+ *
+ * @author Gabriel Breton.
+ */
+public enum Color {
+    
+    RED, GREEN;
+}
+
