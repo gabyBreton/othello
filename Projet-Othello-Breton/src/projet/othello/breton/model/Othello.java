f37ace0bed9d21a57437f178b02417dcc5f43c3f
Gabriel Breton <Breton@43397>
2017-10-22 18:29:19 +0200

    Create the interface of the game.

---
+++ Projet-Othello-Breton/src/projet/othello/breton/model/Othello.java
@@ -0,0 +1,63 @@
+package projet.othello.breton.model;
+
+import java.util.List;
+
+/**
+ * Interface of the facade of Othello.
+ * 
+ * @author Gabriel Breton - 43397
+ */
+interface Othello {
+    
+    /**
+     * Verifies if the game is over.
+     * 
+     * @return true if the game is over, or else false.
+     */
+    boolean isOver();
+    
+    /**
+     * Gives a list with all the players of the game.
+     * 
+     * @return a list with all the players of the game.
+     */
+    List<Players> getPlayers();
+    
+    /**
+     * Gives the current player.
+     * 
+     * @return the current player.
+     */
+    Players getCurrentPlayer();
+    
+    /**
+     * Gives the height of the board.
+     * 
+     * @return the height of the board.
+     */
+    int getHeight();
+    
+    /**
+     * Gives the width of the board.
+     * 
+     * @return the width of the board.
+     */
+    int getWidht();
+    
+    /**
+     * Gives the color of cell at a position.
+     * 
+     * @param x the position on the x axis.
+     * @param y the position on the y axis.
+     * @return the color of the cell, or null if the cell is free.
+     */
+    Color getColor(int x, int y);
+    
+    /**
+     * Place a pawn on a position on the game board.
+     * 
+     * @param x the position on the x axis.
+     * @param y the position on the y axis.
+     */
+    void play(int x, int y);
+}
