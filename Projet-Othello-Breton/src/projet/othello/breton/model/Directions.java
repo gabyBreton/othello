e985e7988871a032bd18f70386284350d7bc1774
Gabriel Breton <Breton@43397>
2017-10-26 20:28:23 +0200

    Update Javadoc.

--- Projet-Othello-Breton/src/projet/othello/breton/model/Directions.java
+++ Projet-Othello-Breton/src/projet/othello/breton/model/Directions.java
@@ -2,30 +2,73 @@ package projet.othello.breton.model;
 
 /**
  * This class provides directions and their movements values on the board.
+ * 
  * @author Gabriel Breton - 43397
  */
 public enum Directions {
+    
+    /**
+     * North.
+     */
     NO (-1, 0),
+    
+    /**
+     * North East.
+     */
     NE (-1, 1),
+    
+    /**
+     * East.
+     */
     EA (0, 1),
+    
+    /**
+     * South East.
+     */
     SE (1, 1), 
+    
+    /**
+     * South.
+     */
     SO (1, 0),
+    
+    /**
+     * South West.
+     */
     SW (1, -1),
+    
+    /**
+     * West.
+     */
     WE (0, -1),
+    
+    /**
+     * North West.
+     */
     NW (-1, -1);
     
-    private int xAxisMov;
-    private int yAxisMov;
+    private final int xAxisMov;
+    private final int yAxisMov;
 
     private Directions(int xAxisMov, int yAxisMov) {
         this.xAxisMov = xAxisMov;
         this.yAxisMov = yAxisMov;
     }
 
+    /**
+     * Gives the movement value on the x axis.
+     * 
+     * @return the movement value on the x axis.
+     */
     public int getxAxisMov() {
         return xAxisMov;
     }
 
+    /**
+     * Gives the movement value on the y axis.
+     * 
+     * @return the movement value on the y axis.
+     */
     public int getyAxisMov() {
         return yAxisMov;
     }
