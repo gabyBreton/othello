2f20e805beb7f7361ba30f5d2920838602e4c3e4
Gabriel Breton <Breton@43397>
2017-10-27 01:49:20 +0200

    Add Commands class to manage the inputs from the users.

---
+++ Projet-Othello-Breton/src/project/othello/breton/viewConsole/Commands.java
@@ -0,0 +1,115 @@
+package project.othello.breton.viewConsole;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import projet.othello.breton.model.OthelloImpl;
+
+/**
+ * This class provides methods to manage the command-line interactions with the
+ * user.
+ *
+ * @author Gabriel Breton - 43397
+ */
+public class Commands {
+
+    /**
+     * Verify the validity of a given command.
+     *
+     * @param command the command that the user have entered.
+     * @return true if the command is valid, else false. A command is valid if
+     * we can use it to show the board, show the score, play a move or display
+     * the help.
+     */
+    public static boolean verifyCommand(String command) {
+        boolean findMatch;
+
+        List<Pattern> patterns = creatPatternList();
+        findMatch = isCmdMatching(patterns, command);
+
+        return findMatch;
+    }
+
+    /**
+     * Create a list with all the regex patterns for the commands matching
+     * verification.
+     *
+     * @return a list with all the patterns.
+     */
+    private static List<Pattern> creatPatternList() {
+
+        List<Pattern> patterns = new ArrayList<>();
+        patterns.add(Pattern.compile("show[\\s]*"));
+        patterns.add(Pattern.compile("score[\\s]*"));
+        patterns.add(Pattern.compile("play[\\s]([\\d]+[\\s]){2}"));
+        patterns.add(Pattern.compile("help[\\s]*"));
+
+        return patterns;
+    }
+
+    /**
+     * Verify if a given command matches with an expected command value.
+     *
+     * @param patterns the differents patterns of regex to verify with the
+     * command.
+     * @param command the command to verify the matching.
+     * @return true if the command matches with one of the patterns, or else
+     * false.
+     */
+    private static boolean isCmdMatching(List<Pattern> patterns, String command){
+        boolean findMatch;
+        findMatch = false;
+
+        for (Pattern pattern : patterns) {
+            Matcher matcher = pattern.matcher(command);
+            if (matcher.find()) {
+                findMatch = true;
+                break;
+            }
+        }
+        return findMatch;
+    }
+    
+        /**
+     * Assign an ID for each type of command. As types of command, we have:
+     * 'show', 'help', 'score' and 'play'.
+     *
+     * @param cmdSplitted An array containing all the words (non-whitespace) of
+     * the command.
+     * @return an ID that correspond to a command.
+     * @throws IllegalArgumentException if the command is not recognized, such
+     * as it is a bad wrong shape or wrong command type.
+     */
+    public static int getCommandId(String[] cmdSplitted)
+            throws IllegalArgumentException {
+        int cmdId;
+
+        if (cmdSplitted[0].matches("show")) {
+            cmdId = 1;
+        } else if (cmdSplitted[0].matches("score")) {
+            cmdId = 2;
+        } else if (cmdSplitted[0].matches("help")) {
+            cmdId = 3;
+        } else if (cmdSplitted[0].matches("play")) {
+            cmdId = 4;
+        } else {
+            throw new IllegalArgumentException("Unrecognized command. Should be"
+                                    + " 'show', 'score', 'help' or 'play x y'");
+        }
+        return cmdId;
+    }
+    
+    /**
+     * Executes a given command.
+     *
+     * @param command the command to execute
+     * @param cmdId the ID of the command type.
+     * @param game the current game session.
+     * @throws IllegalArgumentException if the command type is not valid.
+     */
+    public static void execCommand(String[] command, int cmdId,OthelloImpl game)
+                                               throws IllegalArgumentException {
+        
+    }
+}
