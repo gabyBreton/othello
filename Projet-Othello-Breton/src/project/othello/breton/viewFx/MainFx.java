fcb6e48f71f08b6f8bdc721783dda8eb16fbf6b0
Gabriel Breton <Breton@43397>
2017-11-03 21:01:40 +0100

    Update JavaDoc.

--- Projet-Othello-Breton/src/project/othello/breton/viewFx/MainFx.java
+++ Projet-Othello-Breton/src/project/othello/breton/viewFx/MainFx.java
@@ -8,35 +8,56 @@ import javafx.stage.Stage;
 import project.othello.breton.model.OthelloImpl;
 
 /**
- *
+ * This class is the main class of the GUI implementation of the Othello game.
+ * 
  * @author Gabriel Breton - 43397
  */
 public class MainFx extends Application {
-
     private OthelloImpl game;
 
+    /**
+     * Starts and sets all the components of the game.
+     * @param primaryStage 
+     */
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Othello");
+        primaryStage.setMaxWidth(1000);
+        primaryStage.setMaxHeight(750);
 
-        game = new OthelloImpl(8, 8);
-
-        BorderPane root = new BorderPane();
-        root.setId("pane");
-
-        GridPane board = new BoardPane(game).getBoard();
-        GridPane scoreInfos = new ScoresInfos(game).getSidePane();
-    //    scoreInfos.setGridLinesVisible(true);
-        
-        root.setLeft(board);
-        root.setCenter(scoreInfos);
+        BorderPane root = makeLayout();
         
         Scene scene = new Scene(root);
         scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    /**
+     * Creates a game to creates the differents elements of the GUI.
+     * 
+     * @return a ready to use root.
+     */
+    private BorderPane makeLayout() {
+        BorderPane root = new BorderPane();
+        root.setId("pane");
+     
+        game = new OthelloImpl(8, 8);
+        GridPane board = new BoardPane(game).getBoard();
+        GridPane scoreInfos = new ScoresInfos(game).getSidePane();
+        //    scoreInfos.setGridLinesVisible(true); // FOR DEBUG !
+        
+        root.setLeft(board);
+        root.setCenter(scoreInfos);
+        return root;
+    }
+
+    /**
+     * Launches the game.
+     * 
+     * @param args non used.
+     */
     public static void main(String[] args) {
         launch(args);
     }
