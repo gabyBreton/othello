d31c0567c926fa6503c1b5825b3fce679d1a6a7c
Gabriel Breton <Breton@43397>
2017-11-03 21:07:17 +0100

    Update JavaDoc.

--- Projet-Othello-Breton/src/project/othello/breton/viewFx/Pawn.java
+++ Projet-Othello-Breton/src/project/othello/breton/viewFx/Pawn.java
@@ -6,13 +6,18 @@ import javafx.scene.shape.Ellipse;
 import project.othello.breton.model.PlayerColor;
 
 /**
- *
+ * This class permits to create a pawn.
+ * 
  * @author Gabriel Breton - 43397
  */
 public class Pawn extends StackPane {
-
     private final PlayerColor color;
 
+    /**
+     * Creates a new pawn.
+     * 
+     * @param color the color of the owner of the pawn.
+     */
     public Pawn(PlayerColor color) {
         this.color = color;
 
@@ -23,6 +28,11 @@ public class Pawn extends StackPane {
         getChildren().addAll(bckgrnd, ellipse);
     }
 
+    /**
+     * Creates the upper part of the pawn.
+     * 
+     * @return the upper part of the pawn.
+     */
     private Ellipse makeEllipse() {
         Ellipse ellipse = new Ellipse(24, 20);
         ellipse.setStrokeWidth(2.25);
@@ -31,6 +41,11 @@ public class Pawn extends StackPane {
         return ellipse;
     }
 
+    /**
+     * Creates the lower part of the pawn.
+     * 
+     * @return the lower part of the pawn.
+     */
     private Ellipse makeBackground() {
         Ellipse bckgrnd = new Ellipse(24, 20);
         bckgrnd.setStrokeWidth(2.25);
@@ -39,6 +54,13 @@ public class Pawn extends StackPane {
         return bckgrnd;
     }
 
+    /**
+     * Sets the colors of a pawn.
+     * 
+     * @param color the color of the owner of the pawn.
+     * @param bckgrnd the lower part of the pawn.
+     * @param ellipse the upper part of the pawn.
+     */
     private void setColorsPawn(PlayerColor color, Ellipse bckgrnd, Ellipse ellipse) {
         if (color == PlayerColor.BLACK) {
             bckgrnd.setFill(Color.WHITE);
