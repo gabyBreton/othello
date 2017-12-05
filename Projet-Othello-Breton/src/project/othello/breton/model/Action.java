e7381654274168620177c59818b48831e1e4e12e
Gabriel Breton <Breton@43397>
2017-12-05 23:02:46 +0100

    Update Javadoc

--- Projet-Othello-Breton/src/project/othello/breton/model/Action.java
+++ Projet-Othello-Breton/src/project/othello/breton/model/Action.java
@@ -1,7 +1,8 @@
 package project.othello.breton.model;
 
 /**
- * This class represents an action on the game.
+ * This class is used to represent an action in the game the use it in the
+ * history table view.
  *
  * @author Gabriel Breton - 43397
  */
@@ -13,33 +14,66 @@ public class Action {
     private final String position;
     private final int taking;
 
-    public Action(int id, String pseudo, String action, String position, int taking) {
+    /**
+     * Creates a new action
+     * 
+     * @param id a number to identify the action.
+     * @param color the color of the player who made the action.
+     * @param action a description of the action.
+     * @param position the position on the board of the action.
+     * @param taking the number of pawn taken by the action.
+     */
+    public Action(int id, String color, String action, String position, 
+                  int taking) {
         this.id = id;
-        this.color = pseudo;
+        this.color = color;
         this.action = action;
         this.position = position;
         this.taking = taking;
     }
 
+    /**
+     * Gives the id of the action.
+     * 
+     * @return the id of the action.
+    */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gives the color of the player who made the action.
+     * 
+     * @return the color of the player who made the action.
+     */
     public String getColor() {
         return color;
     }
-
+    
+    /**
+     * Gives the description of the action.
+     * 
+     * @return the description of the action.
+     */
     public String getAction() {
         return action;
     }
 
+    /**
+     * Gives the position on the board of the action.
+     * 
+     * @return the position on the board of the action.
+     */
     public String getPosition() {
         return position;
     }
 
+    /**
+     * Gives the number of pawn taken by the action.
+     * 
+     * @return the number of pawn taken by the action.
+     */
     public int getTaking() {
         return taking;
     }
-    
-    
 }
